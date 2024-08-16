66e28ffab88ffbbcef84fe7db38acd86fd2fbb6d
SHIBAHARA Hiroki <shibomb@gmail.com>
2024-08-16 20:28:48 +0900

    jaをデフォルトにし、サブ言語はenのみにして、一通り動作する構成にしました。

--- src/i18n/const.ts
+++ src/i18n/const.ts
@@ -3,12 +3,15 @@
  * ("en" stands for English.) Also used for fallback pages
  * when they don't exist in a locale.
  */
-export const defaultLocale = "en";
+// export const defaultLocale = "en";
+export const defaultLocale = "ja";
 
 /**
  * All other locales supported by the website.
  */
-export const nonDefaultSupportedLocales = ["es", "hi", "ko", "zh-Hans", "ja"];
+// export const nonDefaultSupportedLocales = ["es", "hi", "ko", "zh-Hans", "ja"];
+// export const nonDefaultSupportedLocales = ["ja"];
+export const nonDefaultSupportedLocales = ["en"];
 
 /**
  * The list of locales (languages) the website
